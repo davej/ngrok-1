124f88fe0718260952503aa1db20d6479a9b7cf6
Dave Jeffery <dave@davejeffery.com>
2015-07-29 15:57:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var emitter = new EventEmitter();
 var tunnels = {};
 var exports = {};
 
-var TUNNEL_OK = /\[INFO\] \[client\] Tunnel established at ((tcp|https)..*.ngrok.com(:[0-9]+)?)/;
-var TUNNEL_BUSY = /\[EROR\] \[client\] Server failed to allocate tunnel: The tunnel ((tcp|http|https)..*.ngrok.com([0-9]+)?) (.*is already registered)/;
+var TUNNEL_OK = /\[INFO\] \[client\] Tunnel established at ((tcp|https)..*.pin.gy(:[0-9]+)?)/;
+var TUNNEL_BUSY = /\[EROR\] \[client\] Server failed to allocate tunnel: The tunnel ((tcp|http|https)..*.pin.gy([0-9]+)?) (.*is already registered)/;
 
 function connect(opts, cb) {
 
